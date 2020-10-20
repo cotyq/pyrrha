b4fabbbd9f644f6b5043605ca149dc408582097c
cotyq <cotii.q@gmail.com>
2020-10-20 17:48:46 -0300

    Initial commit

---
+++ pymetheus/runner.py
@@ -0,0 +1,16 @@
+class Runner:
+    @staticmethod
+    # TODO sacar target_class > Buscar la implementacion de la clase padre de programmed_class
+    def validate_class(programmed_class, target_class, seed=None):
+        # reports = OrderedDict()  # collections
+        #
+        # values = cls.get_random_values()
+        # method = cls(**values)
+        # method.run()
+        # reports[cls] = method.generate_report()
+        # return reports()
+        pass
+
+    @staticmethod
+    def validate_method(programmed_class, target_class, method_name, seed=None):
+        pass
